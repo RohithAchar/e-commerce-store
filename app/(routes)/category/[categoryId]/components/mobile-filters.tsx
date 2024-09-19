f7a9283f7aefafd32cf013a9e29ad021bc6fca7a
RohithAchar <rohithachar2000@gmail.com>
2024-09-19 15:30:55 +0530

    added filter for mobile

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -8,6 +8,8 @@ import { Color, Size } from "@/types";
 import Button from "@/components/ui/button";
 import IconButton from "@/components/ui/icon-button";
 
+import Filter from "./filter";
+
 interface MobileFiltersProps {
   sizes: Size[];
   colors: Color[];
@@ -37,6 +39,11 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({ sizes, colors }) => {
           <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
             <div className="flex items-center justify-end px-4">
               <IconButton icon={<X size={15} onClick={onClose} />} />
+            </div>
+
+            <div className="p-4">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
             </div>
           </Dialog.Panel>
         </div>
