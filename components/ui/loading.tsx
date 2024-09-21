99938fa46ca7ee7a9dc02d3ab0d3d445ee513631
RohithAchar <rohithachar2000@gmail.com>
2024-09-21 09:55:39 +0530

    fixed spinner postion

--- components/ui/loading.tsx
+++ components/ui/loading.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center h-full mt-[20%]">
+    <div className="flex items-center justify-center h-full">
       <div className="w-10 h-10 border-4 border-t-4 border-t-black dark:border-t-white border-indigo-200 dark:border-gray-700 rounded-full animate-spin"></div>
     </div>
   );
