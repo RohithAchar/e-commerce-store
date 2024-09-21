26ac8a894010852da5b559a27cedde02c0954f57
RohithAchar <rohithachar2000@gmail.com>
2024-09-21 09:52:50 +0530

    added spinner

---
+++ components/ui/loading.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-full mt-[20%]">
+      <div className="w-10 h-10 border-4 border-t-4 border-t-black dark:border-t-white border-indigo-200 dark:border-gray-700 rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default Loader;
